369d046a59c740944046fe7b5d4df5e76ad3936e
lucapandolfelli <lucapandolfelli2002@gmai.com>
2022-11-02 21:47:10 -0300

    Clase 36

--- src/daos/carritos/CarritosDaoFirebase.js
+++
@@ -1,9 +0,0 @@
-import ContenedorFirebase from "../../containers/ContenedorFirebase.js";
-
-class CarritosDaoFirebase extends ContenedorFirebase {
-  constructor() {
-    super("carritos");
-  }
-}
-
-export { CarritosDaoFirebase };
