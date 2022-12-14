c0725ff9d6847ee980ff3af8a423c8ba0fecba16
lucapandolfelli <lucapandolfelli2002@gmai.com>
2022-12-14 19:26:07 -0300

    Clase 48

--- src/middleware/jwtAuth.js
+++ src/middleware/jwtAuth.js
@@ -1,15 +1,20 @@
 import jwt from "jsonwebtoken";
+import { logger } from "../utils/index.js";
 
 const JWTAuth = (req, res, next) => {
   const token = req.cookies.token;
   try {
     if (!token) {
-      return res.status(302).redirect("/auth/login");
+      res.tstaus(302);
+      logger.error(`${req.method} ${req.originalUrl} ${res.statusCode}`);
+      return res.redirect("/auth/login");
     }
     const user = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
     req.user = user;
     next();
   } catch (err) {
+    res.status(302);
+    logger.error(`${req.method} ${req.originalUrl} ${res.statusCode}`);
     res.clearCookie("token");
     return res.redirect("/");
   }
